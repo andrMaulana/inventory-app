0de1656334d22c122cc62fb4ef9dfd41d2b8cef9
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-11 10:54:36 +0700

    Feat (database) : Create relation transacktion detail

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -10,4 +10,12 @@ class TransactionDetail extends Model
     use HasFactory;
 
     protected $fillable = ['transaction_id', 'product_id', 'quantity'];
+
+    public function transaction() {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
