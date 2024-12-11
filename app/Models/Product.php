c80ae210b585cd5eb117425834e18a6842d4123d
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-11 10:53:11 +0700

    Feat (database) : Create relation product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,37 @@ class Product extends Model
     protected $fillable = [
         'category_id', 'supplier_id', 'name', 'slug', 'description', 'quantity','image', 'unit'
     ];
+
+    /**
+    * relation categories
+    */
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+    * relation suppliers
+    */
+
+    public function supplier() {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    /**
+    t* relation cstock
+    */
+
+    public function stock(){
+        return $this->hasOne(Stock::class);
+    }
+
+    /**
+    * relation sctocks
+    */
+
+    public function stocks(){
+        return $this->hasMany(Stock::class);
+}
+
 }
