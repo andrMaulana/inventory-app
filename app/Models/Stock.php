76131745765819559dcccb2ad566bcad4ab35364
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-11 10:53:32 +0700

    Feat (database) : Create relation stock

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -10,4 +10,10 @@ class Stock extends Model
     use HasFactory;
 
     protected $fillable = ['product_id', 'type', 'quantity'];
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
+
+
