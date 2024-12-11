aeb9ac55bef2c4815dffd8a0798c6f2054456096
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-11 10:54:00 +0700

    Feat (database) : Create relation transacktion

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,4 +10,12 @@ class Transaction extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'invoice'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function details() {
+        return $this->hasMany(TransactionDetail::class);
+    }
 }
