1cbd250f855d50034f9fef22bd7c1664cf3b611d
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-11 10:54:57 +0700

    Feat (database) : Create relation cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -10,4 +10,12 @@ class Cart extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'product_id', 'quantity'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::clas);
+    }
 }
