2b75c2ec2a8500c2cafd7f4d7f82ba2b29d379eb
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 11:15:41 +0700

    Feat (Database): call traits in supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,17 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\HasScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    use HasFactory;
+    use HasFactory, HasScope;
 
     /**
      * fillable
      */
-    protected $fillable = [
-        'name', 'telp', 'address'
-    ];
+    protected $fillable = ['name', 'telp', 'address'];
 }
