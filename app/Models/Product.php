963e5986a4364f852ad0c8e1f9048dc762167a49
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 11:06:39 +0700

    Feat (Database): add relation product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,4 +15,37 @@ class Product extends Model
     protected $fillable = [
          'category_id', 'supplier_id', 'name', 'slug', 'description', 'quantity', 'image', 'unit'
      ];
+
+    /**
+     * relation categories
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * relation suppliers
+     */
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    /**
+     * relation stocks
+     */
+    public function stock()
+    {
+        return $this->hasOne(Stock::class);
+    }
+
+    /**
+     * relation stocks
+    */
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class);
+    }
+
 }
