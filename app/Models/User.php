9eeddcc47a56484251a0678e7c7506d51da80efd
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 11:15:58 +0700

    Feat (Database): add accessor in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use App\Traits\HasScope;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +12,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasScope;
 
     /**
      * The attributes that are mass assignable.
@@ -49,5 +50,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * accessor avatar users
+     */
+    protected function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($avatar) => $avatar != '' ? asset('/storage/profiles/' . $avatar) : 'https://www.gravatar.com/avatar/b2b58f77632a6f5c46d30b08108baa57?d=mm&s=150',
+        );
     }
 }
