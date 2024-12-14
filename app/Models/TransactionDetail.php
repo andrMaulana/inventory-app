a307027d99fac82c9c9924edf1d7cedd48140028
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 11:07:15 +0700

    Feat (Database): add relation detail

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -13,4 +13,20 @@ class TransactionDetail extends Model
      * fillable
      */
     protected $fillable = ['transaction_id', 'product_id', 'quantity'];
+
+    /**
+     * relation transactions
+     */
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    /**
+     * relation products
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
