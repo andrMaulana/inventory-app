8864c432819212f518b8a86d711cff4fa425f416
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 11:14:33 +0700

    Feat (Database): add accessor in category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,23 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\HasScope;
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug, HasScope;
 
     /**
      * fillable
      */
     protected $fillable = ['name', 'slug', 'image'];
 
-		/**
-		*	relation products
-		*/
-        public function products()
-        {
-            return $this->hasMany(Product::class);
-        }
+    /**
+     * accessor category image
+     */
+    protected function image() : Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => asset('storage/categories/'. $image)
+        );
+    }
+
+    /**
+     * relation products
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
