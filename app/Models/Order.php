552e46eaca3930ef03058a576305745d59c8080d
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 11:16:15 +0700

    Feat (Database): call traits in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,17 +2,27 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatus;
+use App\Traits\HasScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, HasScope;
 
     /**
      * fillable
      */
     protected $fillable = ['user_id', 'product_id', 'quantity', 'status'];
+
+    /**
+     * casts
+     */
+    protected $casts = [
+        'status' => OrderStatus::class,
+    ];
+
 
     /**
      * relation users
