5e64e67f0fd565f9805f2dffcea83f7b1fac19cf
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 10:43:16 +0700

    Feat (Database): create product model

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     */
+    protected $fillable = [
+         'category_id', 'supplier_id', 'name', 'slug', 'description', 'quantity', 'image', 'unit'
+     ];
+}
