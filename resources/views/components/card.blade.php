7ecd4da577f5d1d530d6b26c7f34db056b66e3ac
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-14 10:25:30 +0700

    Feat (Setup): add component card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+@props(['title' => ''])
+<div class="card rounded-lg border-0">
+    <div class="card-header">
+        <h3 class="card-title">{{ $title }}</h3>
+    </div>
+
+    <div {{ $attributes->merge(['class' => '']) }}>
+        {{ $slot }}
+    </div>
+</div>
