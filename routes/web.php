f1f283741d6a87d0b0b4dc683f9ab830928b60c7
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-16 01:52:21 +0700

    Feat (Admin): register route admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apps\DashboardController;
 
+// home
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'apps', 'as' => 'apps.', 'middleware' => ['auth']], function(){
+    // dashboard
+    Route::get('dashboard', DashboardController::class)->name('dashboard');
 });
