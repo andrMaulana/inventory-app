1ba4a1a9a5023bdf4330b9ead186f21b143e54d1
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-16 16:33:22 +0700

    Feat (permissionPage): add create data permission

--- resources/views/pages/apps/permissions/index.blade.php
+++ resources/views/pages/apps/permissions/index.blade.php
@@ -17,7 +17,8 @@
                             <tr>
                                 <td>{{ $i + $permissions->firstItem() }}</td>
                                 <td>{{ $permission->name }}</td>
-                                <td></td>
+                                <td>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -25,5 +26,21 @@
             </x-card-action>
             <div class="d-flex justify-content-end">{{ $permissions->links() }}</div>
         </div>
+        @can('permissions-create')
+            <div class="col-12 col-lg-4">
+                <form action="{{ route('apps.permissions.store') }}" method="POST">
+                    @csrf
+                    <x-card title="Tambah Data Permission" class="card-body">
+                        <x-input
+                            title="Nama Permission"
+                            name="name" type="text"
+                            placeholder="Masukan nama permission"
+                            value="{{ old('name') }}"
+                        />
+                        <x-button title="Simpan" type="save"/>
+                    </x-card>
+                </form>
+            </div>
+        @endcan
     </x-container>
 @endsection
