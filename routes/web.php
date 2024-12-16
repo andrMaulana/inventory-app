9f1b26183f512900fb1c67c3d37ce3cbb1b2e587
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-16 16:26:38 +0700

    Feat (permissionPage): register route permission page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Apps\DashboardController;
+use App\Http\Controllers\Apps\PermissionController;
 
-// home
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,4 +11,6 @@ Route::get('/', function () {
 Route::group(['prefix' => 'apps', 'as' => 'apps.', 'middleware' => ['auth']], function(){
     // dashboard
     Route::get('dashboard', DashboardController::class)->name('dashboard');
+    // permissions
+    Route::resource('permissions', PermissionController::class);
 });
