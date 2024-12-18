f5ad38f36dbcbaf5247ff17a0cbe38765c4f4eee
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-18 23:22:46 +0700

    Feat (Roles): Add navigation role in sidebar dashbboard

--- resources/views/layouts/apps/partials/sidebar.blade.php
+++ resources/views/layouts/apps/partials/sidebar.blade.php
@@ -233,24 +233,24 @@
                     </li>
                 @endcan
                 @can('roles-access')
-                    <li class="nav-item">
-                        <a class="nav-link" href="">
-                            <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-check"
-                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
-                                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <circle cx="9" cy="7" r="4"></circle>
-                                    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
-                                    <path d="M16 11l2 2l4 -4"></path>
-                                </svg>
-                            </span>
-                            <span class="nav-link-title">
-                                Role
-                            </span>
-                        </a>
-                    </li>
-                @endcan
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('apps.roles*') ? 'text-cyan' : '' }}" href="{{ route('apps.roles.index') }}">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-check"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <circle cx="9" cy="7" r="4"></circle>
+                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                                <path d="M16 11l2 2l4 -4"></path>
+                            </svg>
+                        </span>
+                        <span class="nav-link-title">
+                            Role
+                        </span>
+                    </a>
+                  </li>
+                  @endcan
                 @can('users-access')
                     <li class="nav-item">
                         <a class="nav-link" href="">
