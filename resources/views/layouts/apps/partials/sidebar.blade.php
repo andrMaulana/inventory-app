cdf686a9ce279291008673e176afbdf117aed17d
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-22 01:37:09 +0700

    feat(user): :sparkles: add navigation user in sidebar

--- resources/views/layouts/apps/partials/sidebar.blade.php
+++ resources/views/layouts/apps/partials/sidebar.blade.php
@@ -252,23 +252,23 @@
                   </li>
                   @endcan
                 @can('users-access')
-                    <li class="nav-item">
-                        <a class="nav-link" href="">
-                            <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle"
-                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
-                                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <circle cx="12" cy="12" r="9"></circle>
-                                    <circle cx="12" cy="10" r="3"></circle>
-                                    <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
-                                </svg>
-                            </span>
-                            <span class="nav-link-title">
-                                User
-                            </span>
-                        </a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('apps.users*') ? 'text-cyan' : '' }}" href="{{ route('apps.users.index') }}">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <circle cx="12" cy="12" r="9"></circle>
+                                <circle cx="12" cy="10" r="3"></circle>
+                                <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
+                            </svg>
+                        </span>
+                        <span class="nav-link-title">
+                            User
+                        </span>
+                    </a>
+                  </li>
                 @endcan
                 @can('reports-access')
                     <div class="hr-text hr-text-left ml-2 mb-2 mt-2">Laporan</div>
