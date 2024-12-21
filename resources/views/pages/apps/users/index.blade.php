356061632b0855e3df08cf869dafcccf9b499d47
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-22 01:43:41 +0700

    feat(user): :sparkles: create view update

--- resources/views/pages/apps/users/index.blade.php
+++ resources/views/pages/apps/users/index.blade.php
@@ -26,14 +26,52 @@
                                     @endforeach
                                 </td>
                                 <td>
-                                    @can('users-delete')
-                                        <x-button
-                                            title="Hapus Data"
-                                            type="delete"
-                                            :id="$user->id"
-                                            :url="route('apps.users.destroy', $user->id)"
-                                        />
-                                    @endcan
+                                    <x-button
+                                        title="Ubah Role"
+                                        type="modal"
+                                        :id="$user->id"
+                                    />
+                                    <x-modal
+                                        title="Ubah Role"
+                                        :id="$user->id"
+                                    >
+                                        <form action="{{ route('apps.users.update', $user->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <x-input
+                                                title="Nama"
+                                                name="name"
+                                                type="text"
+                                                :value="$user->name"
+                                                disabled
+                                            />
+                                            <x-input
+                                                title="Email"
+                                                name="name"
+                                                type="email"
+                                                :value="$user->email"
+                                                disabled
+                                            />
+                                            <x-select-group title="Roles">
+                                                @foreach ($roles as $role)
+                                                    <label class="form-selectgroup-item">
+                                                        <input type="checkbox" name="roles[]" value="{{ $role->name }}"
+                                                            class="form-selectgroup-input" @checked($user->roles->find($role->id))>
+                                                        <span class="form-selectgroup-label">
+                                                            {{ $role->name }}
+                                                        </span>
+                                                    </label>
+                                                @endforeach
+                                            </x-select-group>
+                                            <x-button title="Simpan" type="save"/>
+                                        </form>
+                                    </x-modal>
+                                    <x-button
+                                        title="Hapus Data"
+                                        type="delete"
+                                        :id="$user->id"
+                                        :url="route('apps.users.destroy', $user->id)"
+                                    />
                                 </td>
                             </tr>
                         @endforeach
