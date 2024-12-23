f78dc741d130f08613a159394cfd90db89f9c1d6
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-23 21:47:19 +0700

    feat(category): :sparkles: add navigation category at sidebar

--- resources/views/layouts/apps/partials/sidebar.blade.php
+++ resources/views/layouts/apps/partials/sidebar.blade.php
@@ -61,24 +61,24 @@
                 <div class="hr-text hr-text-left ml-2 mb-2 mt-2">Master Data</div>
                 @endcanany
                 @can('categories-access')
-                    <li class="nav-item">
-                        <a class="nav-link" href="">
-                            <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-category-2"
-                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
-                                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M14 4h6v6h-6z"></path>
-                                    <path d="M4 14h6v6h-6z"></path>
-                                    <circle cx="17" cy="17" r="3"></circle>
-                                    <circle cx="7" cy="7" r="3"></circle>
-                                </svg>
-                            </span>
-                            <span class="nav-link-title">
-                                Kategori
-                            </span>
-                        </a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('apps.categories*') ? 'text-cyan' : '' }}" href="{{ route('apps.categories.index') }}">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-category-2"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M14 4h6v6h-6z"></path>
+                                <path d="M4 14h6v6h-6z"></path>
+                                <circle cx="17" cy="17" r="3"></circle>
+                                <circle cx="7" cy="7" r="3"></circle>
+                            </svg>
+                        </span>
+                        <span class="nav-link-title">
+                            Kategori
+                        </span>
+                    </a>
+                </li>
                 @endcan
                 @can('suppliers-access')
                     <li class="nav-item">
