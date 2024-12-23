9dbbf77581c3b0c0b9ef493f7e0ea36bc379df3a
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-23 21:46:16 +0700

    feat(category): :sparkles: register route category

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Apps\DashboardController;
-use App\Http\Controllers\Apps\PermissionController;
 use App\Http\Controllers\Apps\RoleController;
 use App\Http\Controllers\Apps\UserController;
+use App\Http\Controllers\Apps\CategoryController;
+use App\Http\Controllers\Apps\DashboardController;
+use App\Http\Controllers\Apps\PermissionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,6 +14,8 @@ Route::get('/', function () {
 Route::group(['prefix' => 'apps', 'as' => 'apps.', 'middleware' => ['auth']], function(){
     // dashboard
     Route::get('dashboard', DashboardController::class)->name('dashboard');
+    // categories
+    Route::resource('categories', CategoryController::class)->except(['show']);
     // permissions
     Route::resource('permissions', PermissionController::class)->except(['create', 'edit', 'show']);
     // roles
