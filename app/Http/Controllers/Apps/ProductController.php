185fe098470d0279e010ba536676cd001c773a25
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-01 16:24:25 +0700

    feat(product): :sparkles: use models category at controller

--- app/Http/Controllers/Apps/ProductController.php
+++ app/Http/Controllers/Apps/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Apps;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
+
 
 class ProductController extends Controller
 {
