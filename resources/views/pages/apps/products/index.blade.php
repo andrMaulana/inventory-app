a6c54e2e9564cd0c6956dcaa1a044201e5fb2597
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-01 17:33:49 +0700

    feat(product): :sparkles: create view index product

---
+++ resources/views/pages/apps/products/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.apps.master', ['title' => 'Data Produk'])
+
+@section('content')
+    <x-container>
+        <div class="col-12">
+            @can('products-create')
+                <x-button
+                    title="Tambah Data Produk"
+                    type="create"
+                    :url="route('apps.products.create')"
+                />
+            @endcan
+            <x-card-action title="Daftar Produk" url="{{ route('apps.products.index') }}">
+                <x-table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Gambar</th>
+                            <th>Nama Produk</th>
+                            <th>Supplier</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $i => $product)
+                            <tr>
+                                <td>{{ $i + $products->firstItem() }}</td>
+                                <td>
+                                    <span class="avatar rounded avatar-lg" style="background-image: url({{ $product->image }})"></span>
+                                </td>
+                                <td>
+                                    {{ $product->name }}
+                                    <div>
+                                        <small class="text-secondary">{{ $product->category->name }}</small>
+                                    </div>
+                                </td>
+                                <td></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </x-table>
+            </x-card-action>
+            <div class="d-flex justify-content-end">{{ $products->links() }}</div>
+        </div>
+    </x-container>
+@endsection
