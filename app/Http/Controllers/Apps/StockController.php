99d5bac3409fb8443a16fd810030cf92859d0d8a
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-03 19:32:34 +0700

    feat(stock): :sparkles: impl method middleware

--- app/Http/Controllers/Apps/StockController.php
+++ app/Http/Controllers/Apps/StockController.php
@@ -8,7 +8,16 @@ use App\Models\Product;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Routing\Controllers\HasMiddleware;
 
-class StockController extends Controller
+class StockController extends Controller implements HasMiddleware
 {
-    //
+    /**
+     * middleware
+     */
+    public static function middleware()
+    {
+        return [
+            new Middleware('permission:stocks-access', only : ['index']),
+            new Middleware('permission:stocks-create', only : ['store'])
+        ];
+    }
 }
