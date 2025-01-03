c08e43a49318b93f1804c4287f6a408a2f14b477
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-03 19:32:12 +0700

    feat(stock): :sparkles: add library

--- app/Http/Controllers/Apps/StockController.php
+++ app/Http/Controllers/Apps/StockController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Apps;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
 class StockController extends Controller
 {
