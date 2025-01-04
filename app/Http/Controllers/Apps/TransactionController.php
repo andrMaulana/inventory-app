a83762f51ec531dde117e1ccfa9db3c18bd3f24c
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-04 19:35:34 +0700

    feat(transaction): :sparkles: impl method __invoke

--- app/Http/Controllers/Apps/TransactionController.php
+++ app/Http/Controllers/Apps/TransactionController.php
@@ -26,6 +26,32 @@ class TransactionController extends Controller implements HasMiddleware
      */
     public function __invoke(Request $request)
     {
-        //
+        // check permission users
+        if ($request->user()->can('transactions-admin')) {
+            // get all transaction data with paginate
+            $transactions = Transaction::with(['details' => function($query) {
+                $query->sum('quantity');
+            }])
+            ->search('invoice')
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
+        }elseif($request->user()->can('transactions-users')){
+            // get all transaction by user with paginate
+            $transactions = Transaction::with('details')
+                ->whereUserId($request->user()->id())
+                ->latest()
+                ->paginate(10)
+                ->withQueryString();
+        }
+
+        // add new column in collection transactions total_quantity
+        $transactions->getCollection()->transform(function($transaction){
+            $transaction->total_quantity = $transaction->details->sum('quantity');
+            return $transaction;
+        });
+
+        // render view
+        return view('pages.apps.transaction', compact('transactions'));
     }
 }
