0e3cac0ce4c713f7e3e8df2dbff49a6902bf253e
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-04 19:22:09 +0700

    feat(transaction): :sparkles: create impl interface hasMiddleware

--- app/Http/Controllers/Apps/TransactionController.php
+++ app/Http/Controllers/Apps/TransactionController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Apps;
 
+use App\Models\Transaction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
-class TransactionController extends Controller
+class TransactionController extends Controller implements HasMiddleware
 {
     /**
      * Handle the incoming request.
