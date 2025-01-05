1150473d8c1e7c43fe2166c7f30b7389ad106742
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-05 22:28:24 +0700

    feat(order): :sparkles: import model & dependecy

--- app/Http/Controllers/Apps/OrderController.php
+++ app/Http/Controllers/Apps/OrderController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Apps;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Stock;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Supplier;
+use App\Enums\OrderStatus;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
