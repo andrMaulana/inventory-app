6c9bcea447bec980a4f051e8cdc715d0bf310f1b
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-05 22:29:44 +0700

    feat(order): :sparkles: impl method index

--- app/Http/Controllers/Apps/OrderController.php
+++ app/Http/Controllers/Apps/OrderController.php
@@ -13,5 +13,35 @@ use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
-    //
+     /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // product data
+        $products = Product::select('id', 'name')->orderBy('name')->get();
+
+        // check permissions users
+        if($request->user()->can('orders-admin')){
+            // get all orders data with paginate
+            $orders = Order::with(['product' => function($query){
+                $query->with(['stock' => function($query){
+                    $query->selectRaw('product_id, SUM(CASE WHEN type = "in" THEN quantity ELSE quantity*-1 END) as stock')
+                    ->groupBy('product_id');
+                }]);
+            }, 'user'])
+                ->latest()
+                ->paginate(10)
+                ->withQueryString();
+        }elseif($request->user()->can('orders-users')){
+            // get all orders by user id with paginate
+            $orders = Order::with('product', 'user')
+                ->whereUserId($request->user()->id)
+                ->latest()
+                ->paginate(10)->withQueryString();
+        }
+
+        // render view
+        return view('pages.apps.orders.index', compact('orders', 'products'));
+    }
 }
