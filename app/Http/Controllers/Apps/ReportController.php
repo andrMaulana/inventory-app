b1c630fbf489071c6d78dcc2f005da9ff5927187
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-06 19:48:52 +0700

    chore(report): 🛠 import necessary classes in ReportController
    - Added imports for PDF, Carbon, Stock, and Product models.\n- Included middleware support using HasMiddleware and Middleware traits.

--- app/Http/Controllers/Apps/ReportController.php
+++ app/Http/Controllers/Apps/ReportController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Apps;
 
 use PDF;
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\Stock;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
 class ReportController extends Controller
 {
