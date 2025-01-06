7d64ed6ad523ded48995e11c03b97500313102f1
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-06 20:07:24 +0700

    feat(report): 🗂️ add getFirstStock method in ReportController
    - Added a private method `getFirstStock` to retrieve the first stock before a given date.
    - The method filters stock data using a date condition.
    - Loops through the product data and returns the first stock level with its corresponding date.
    - Provides the initial stock value of each product before the specified date.

--- app/Http/Controllers/Apps/ReportController.php
+++ app/Http/Controllers/Apps/ReportController.php
@@ -48,4 +48,32 @@ class ReportController extends Controller implements HasMiddleware
         return $reports;
     }
 
+    /**
+    * getFirstStock
+    */
+    private function getFirstStock($from_date)
+    {
+        $get_first_stocks = Product::with(['stock' => function($query) use($from_date){
+            $query->selectRaw('product_id, created_at, SUM(CASE WHEN type = "in" THEN quantity ELSE quantity*-1 END) as stock')
+                ->whereDate('created_at', '<', $from_date)
+                ->groupBy('product_id');
+        }])->get();
+
+        // first stock
+        $first_stock = [];
+
+        // loop first stock product data
+        foreach($get_first_stocks as $item){
+            // check if stock is not null
+            if($item->stock != null)
+                // assign first stock with stock
+                $first_stock[$item->id] = [
+                    'stok' => $item->stock->stock,
+                    'date' => $item->stock->created_at->format('Y-m-d'),
+                ];
+        }
+
+        return $first_stock;
+    }
+
 }
