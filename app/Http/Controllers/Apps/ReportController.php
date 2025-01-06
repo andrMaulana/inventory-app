4c2ffe683a93ccca619aa3927fb290fac1aac201
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-06 19:41:19 +0700

    feat(report): :sparkles: add ReportController
    - Created a new controller to handle report-related functionalities.\n- Includes initial setup for methods like index and show.

---
+++ app/Http/Controllers/Apps/ReportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Apps;
+
+use PDF;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    //
+}
