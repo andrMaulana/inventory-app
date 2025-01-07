be211408c3501f89685241beb95a03b0173ebb3b
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-07 19:10:47 +0700

    feat(cart): 🛒 add index method to display cart data with total quantity

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -9,5 +9,18 @@ use App\Models\Product;
 
 class CartController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        // get all carts data
+        $carts = Cart::with('product')
+            ->latest()
+            ->whereUserId($request->user()->id)
+            ->get();
+
+        // sum total quantity data
+        $grandQuantity = $carts->sum('quantity');
+
+        // render view
+        return view('pages.web.carts.index', compact('carts', 'grandQuantity'));
+    }
 }
