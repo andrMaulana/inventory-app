d76ee9416075ed9ed3d17eaf528dde07bd2dba1b
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-07 19:12:03 +0700

    feat(cart): 🛒 add store method to add product to the cart

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -23,4 +23,27 @@ class CartController extends Controller
         // render view
         return view('pages.web.carts.index', compact('carts', 'grandQuantity'));
     }
+
+    public function store(Request $request, Product $product)
+    {
+        // get item if already in cart
+        $alreadyInCart = Cart::with('product')
+            ->whereUserId($request->user()->id)
+            ->whereProductId($product->id)
+            ->first();
+
+        if($alreadyInCart){
+            // render view
+            return back()->with('toast_error', 'Produk sudah ada didalam keranjang');
+        }else{
+            // create new cart data
+            $request->user()->carts()->create([
+                'product_id' => $product->id,
+                'quantity' => '1',
+            ]);
+
+            // render view
+            return redirect(route('cart.index'))->with('toast_success', 'Produk berhasil ditambahkan keranjang');
+        }
+    }
 }
