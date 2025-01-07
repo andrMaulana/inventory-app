b09d49ed1a7fb26d04f859403d8fe6479e8bd5bc
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-07 19:09:33 +0700

    feat(cart): 🛠️ add Cart and Product models to CartController

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Models\Product;
 
 class CartController extends Controller
 {
