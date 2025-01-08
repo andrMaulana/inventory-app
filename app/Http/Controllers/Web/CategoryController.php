6385c9bbf39764f24157d53f93a87756ee7805ac
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-08 20:50:42 +0700

    feat(home): ✨ add index method to fetch and display category data with product count

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        // get all category data
+        $categories = Category::withCount('products')
+            ->latest()
+            ->search('name')
+            ->get();
+
+        // render view
+        return view('web.categories.index', compact('categories'));
+    }
 }
