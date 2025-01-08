cdc6abbdf7e5f64acb51f7dc223fe03b70025753
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-08 20:40:05 +0700

    feat(home): ✨ implement product and category data retrieval in HomeController

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
@@ -12,6 +14,17 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+      // get all products data with pagination
+    $products = Product::with(['category', 'supplier', 'stock' => function($query){
+        $query->selectRaw('product_id, SUM(CASE WHEN type = "in" THEN quantity ELSE quantity*-1 END) as stock')
+        ->groupBy('product_id');
+    }])->search('name')->latest()->paginate(9)->withQueryString();
+
+    // get all categories data with pagination
+    $categories = Category::withCount('products')
+        ->latest()
+        ->paginate(9)->withQueryString();
+
+    return view('web.home', compact('products', 'categories'));
     }
 }
