651665d651194e4177f8c0ca91ba7799e8909ef1
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-08 20:56:52 +0700

    feat(home): ✨ implement logic for show method to fetch products by category

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -22,6 +23,17 @@ class CategoryController extends Controller
 
     public function show(Category $category)
     {
+        // get produts by category data
+        $products = Product::with(['category', 'supplier', 'stock' => function($query){
+            $query->selectRaw('product_id, SUM(CASE WHEN type = "in" THEN quantity ELSE quantity*-1 END) as stock')
+                ->groupBy('product_id');
+            }])
+            ->where('category_id', $category->id)
+            ->latest()
+            ->search('name')
+            ->get();
 
+        // render view
+        return view('web.categories.show', compact('products', 'category'));
     }
 }
